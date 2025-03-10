6164dd8905e46bb72c9108fe3ddb781370796749
Elara <elaramendes@yahoo.com>
2025-03-10 15:35:16 -0300

    Created a simple request url with static content to test

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template
 
-app = Flask("Webpage")
+app = Flask(__name__)
 
-@app.route("/home")
+@app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+@app.route("/api/v1/<station>/<date>/")
+def request(station, date):
+    temperature = 34
+    data = {
+        "station": station,
+        "date": date,
+        "temperature": temperature
+    }
+    return data
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
