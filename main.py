f3517083f39e0802e70c453b86027c3eb8ddf526
Elara <elaramendes@yahoo.com>
2025-03-12 19:24:26 -0300

    Show data table on webpage

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ import pandas as pd
 
 app = Flask(__name__)
 
+stations_df = pd.read_csv("data-small/stations.txt", skiprows=17)
+id_name = stations_df[["STAID", "STANAME                                 "]]
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=id_name.to_html())
 
 
 @app.route("/api/v1/<station>/<date>/")
 def request(station, date):
     filepath = "data-small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filepath, skiprows=20, parse_dates=["    DATE"])
-    stations_df = pd.read_csv("data-small/stations.txt", skiprows=17)
-    # print(df)
     name = stations_df.loc[stations_df["STAID"] == int(station)]["STANAME                                 "].values[0]
     temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     data = {
